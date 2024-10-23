693729bdf971cc6ae3071720a071b247e0a12f07
elisheva <eli65193@gmail.com>
2024-10-23 22:20:29 +0300

    init server

--- daily-music/src/App.js
+++ daily-music/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import backgroundImage from './assets/images/vecteezy_music-notes-background-illustration_24294799.jpg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/data');
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="background"  style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <a href={item.url}>{item.url}</a>: {item.info}
+          </li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
