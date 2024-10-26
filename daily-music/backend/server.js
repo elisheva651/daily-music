5afe95b6b9ae13b601be6fd5eb2b5f9814b008e8
elisheva <eli65193@gmail.com>
2024-10-26 16:06:08 +0300

    changes

--- daily-music/backend/server.js
+++ daily-music/backend/server.js
@@ -76,6 +76,6 @@ app.get('/api/composer', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,  "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
